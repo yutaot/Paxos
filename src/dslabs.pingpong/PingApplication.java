9e7a84b33eedc21caee26b7abf6a7e71881333f5
Yuta <38776947+yutaot@users.noreply.github.com>
2024-05-30 01:45:30 -0700

    Add files via upload

---
+++ src/dslabs.pingpong/PingApplication.java
@@ -0,0 +1,41 @@
+package dslabs.pingpong;
+
+import dslabs.framework.Application;
+import dslabs.framework.Command;
+import dslabs.framework.Result;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+public class PingApplication implements Application {
+    
+    public PingApplication() {}
+
+    // copy constructor
+    public PingApplication(PingApplication application) {}
+
+    @Data
+    public static final class Ping implements Command {
+        @NonNull private final String value;
+    }
+
+    @Data
+    public static final class Pong implements Result {
+        @NonNull private final String value;
+    }
+
+    @Override
+    public Pong execute(Command command) {
+        //Check command validity; Generate result
+        if (!(command instanceof Ping)) {
+            throw new IllegalArgumentException();
+        }
+
+        Ping p = (Ping) command;
+
+        return new Pong(p.value());
+    }
+}
